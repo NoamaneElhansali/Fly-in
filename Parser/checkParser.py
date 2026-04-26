8403bcd51906af184ddaa0b5055d277499cc5403
Noamane Elhansali <noamaneelhansali@gmail.com>
2026-04-26 23:45:04 +0100

    struct file

--- Parser/checkParser.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field, model_validator
-
-
-class CheckParser(BaseModel):
-    nb_drones: int = Field(..., gt=0)
-    start_hub: str = Field(...)
-    end_hub: str = Field(...)
-    
